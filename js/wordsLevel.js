57f28f97d4ac8ea4be00ce23295434d4528e68ac
Sarah-Bourgeois <sarah@nudgeapm.io>
2017-06-07 19:12:11 +0200

    refactoring

--- js/wordsLevel.js
+++ js/wordsLevel.js
@@ -2,16 +2,22 @@
 const elementList = document.querySelectorAll("#lettersList");
 var level = document.getElementById("level");
 // global variables
-var letters_id, keyboard_id;
-var score =0, number_life=3, diminish_timebar=100;
-var temp;
-var count =0;
+var letters_id, keyboard_id, temp;
+var score =0, number_life=3, diminish_timebar=100, count=0;
+var wordLength = 0; // Init de word index
 var listLetters = [];
-var wordLength = 0; // Init de l'index du mot (Permettra de parcourir le mot depuis la fin)
+var initWordLength = lettersList.innerHTML.length; // length of the start word
 
- var initWordLength = lettersList.innerHTML.length; // Taille du mot au depart
-// call function to launch the game
+
+// ===================================
+//  LAUNCH GAME FUNCTION
+// ===================================
 randomWords();
+
+
+// ===================================
+//  The timebar
+// ===================================
 
 // init timebar 
 function time(){
@@ -19,7 +25,19 @@ clearTimeout(temp);
 temp = setInterval(gameTimer, 100); 
 }
 
-// remplace onkeydown="retrieveKey(event)"
+// change size of the timer bar
+function gameTimer() {
+document.getElementById("time").style.width = diminish_timebar + "%";
+diminish_timebar--;
+time();
+}
+
+
+// ===================================
+//  Caption of user is typing 
+// ===================================
+
+// function to catch what is typing
 $( "body").keydown(function(event) {
   retrieveKey(event); 
 });
@@ -32,12 +50,9 @@ time();
 }
 
 
-// change size of the timer bar
-function gameTimer() {
-document.getElementById("time").style.width = diminish_timebar + "%";
-diminish_timebar--;
-time();
-}
+// ==============================
+//  The words 
+// ===============================
 
 // random words
 function randomWords(){
@@ -52,7 +67,7 @@ function randomWords(){
     initWordLength = lettersList.innerHTML.length;
 }
 
-// function which change letter 
+// function which change color letter 
 String.prototype.replaceAt=function(index, replacement) {
     return this.substr(0, index) + replacement+ this.substr(index + replacement.length);
 }
@@ -60,6 +75,8 @@ function setCharAt(str,index,chr) {
     if(index > str.length-1) return str;
     return str.substr(0,index) + chr + str.substr(index+1);
 }
+
+// compare between user is typing and the word
 function wordsComparaison() {
   wordLength = lettersList.innerHTML.length; //retrieve lenght of the div word
      if(keyboard_id == listLetters[count].charCodeAt(0)) {
@@ -70,13 +87,17 @@ function wordsComparaison() {
        if (listLetters.length == count) {
            randomWords();
            count =0;
-       } 
-        
-}
+       }       
+    }
+
+
+// =========================
+//  AUDIO 
+// ==========================
 
 // audio function to start
 $( "#player" ).click(function() { // = function play(idPlayer,btn) {
-  if ( $("#audioPlayer")[0].paused == false) {  // = if (!player.paused) 
+  if ($("#audioPlayer")[0].paused == false) {  // = if (!player.paused) 
       $("#audioPlayer")[0].pause();
       $(this).html('<img src="image/pause.png"/>');
   } else {
